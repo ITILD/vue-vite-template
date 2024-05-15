1b6edce4343a08c9af6c70898171c0dcb0b9da5e
lpp <geolifestudy@gmail.com>
2024-05-15 17:29:50 +0800

    框架逻辑

--- pc-py/test/main.py
+++ pc-py/test/main.py
@@ -1,7 +1,12 @@
 import webview
+from Routes import route,Api
+@route("/home")
+def home(name: str):
+    return f"Hello, {name}!"
 
 if __name__ == '__main__':
+    api = Api()
     # Create a resizable webview window with minimum size constraints
-    webview.create_window('Frameless window', 'http://pywebview.flowrl.com/hello',
-                          frameless=True, easy_drag=True)
+    window = webview.create_window('pywebview中html和python交互的例子', 'index.html', js_api=api)
     webview.start()
+    # ssl=True
