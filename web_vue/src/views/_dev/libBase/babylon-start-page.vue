c24d3f91133d38a7fa01712d9523d14f43cbe293
lpp <geolifestudy@gmail.com>
2024-07-23 17:19:14 +0800

    更新babylon示例

--- web_vue/src/views/_dev/libBase/babylon-start-page.vue
+++ web_vue/src/views/_dev/libBase/babylon-start-page.vue
@@ -36,6 +36,8 @@ const initMap = () => {
 
   // Create a basic BJS Scene object 创建一个场景scene
   scene = new BABYLON.Scene(engine)
+  // 背景透明
+  scene.clearColor = new BABYLON.Color4(0, 0, 0, 0)
   // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}  添加一个相机，并绑定鼠标事件
   camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene)
   // Target the camera to scene origin
@@ -44,17 +46,20 @@ const initMap = () => {
   camera.attachControl(canvas, false)
   // Create a basic light, aiming 0, 1, 0 - meaning, to the sky 添加一组灯光到场景
   const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
-  const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene)
+  // const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene)
   //添加一个球体到场景中
   // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE)
+  const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', { diameter: 2, segments: 32 }, scene)
   // Move the sphere upward 1/2 of its height
   sphere.position.y = 1
   // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false)
+  const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 6, height: 6 }, scene)
+  // 设置ground颜色
+  ground.material = new BABYLON.StandardMaterial('ground', scene)
+  ;(ground.material as BABYLON.StandardMaterial).diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5)
   // Return the created scene
   // run the render loop 最后一步调用engine的runRenderLoop方案，执行scene.render()，让我们的3d场景渲染起来
-  engine.runRenderLoop(function () {
+  engine.runRenderLoop(() => {
     scene.render()
   })
   // the canvas/window resize event handler 监听浏览器改变大小的事件，通过调用engine.resize()来自适应窗口大小
@@ -64,13 +69,13 @@ const initMap = () => {
 
   // 监听元素变化
   let tempSetTime: number
-  const resizeObserver = new ResizeObserver(() =>{
-    tempSetTime && clearTimeout(tempSetTime);
+  const resizeObserver = new ResizeObserver(() => {
+    tempSetTime && clearTimeout(tempSetTime)
     tempSetTime = setTimeout(() => {
       engine.resize()
-    }, 15);
+    }, 15)
   })
-  resizeObserver.observe(document.getElementById('canvasP')as HTMLElement)
+  resizeObserver.observe(document.getElementById('canvasP') as HTMLElement)
   //// resizeObserver.unobserve(canvasP)// 取消监听元素
 }
 </script>
