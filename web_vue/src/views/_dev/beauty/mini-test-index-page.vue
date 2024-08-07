38bd8d5cdf90d42ee395d292b49acc00e1736ead
lpp <geolifestudy@gmail.com>
2024-08-07 17:08:29 +0800

    完整动态加载,防止复杂组件卡顿

--- web_vue/src/views/_dev/beauty/mini-test-index-page.vue
+++ web_vue/src/views/_dev/beauty/mini-test-index-page.vue
@@ -1,14 +1,12 @@
 <template>
     <div flex h-full>
         <ul border-4 border-blue w-20 h-full>
-            <li v-for="(item, index) in dataAll" :key="item.key" m-2 flex items-center @click="clickDataItem(item, index)">
-                <div :class="item.key === dataSlice.active && 'bg-deep-4'">{{ item.name }}</div>
-                <!-- <a :href="'#' + item.key" :class="item.key === dataSlice.active && 'bg-deep-4'"
-                    >{{ item.name }}</a> -->
+            <li v-for="(item, index) in dataAll" :key="item.id" m-2 flex items-center @click="clickDataItem(item, index)">
+                <span :class="item.id === dataSlice.active && 'bg-deep-4'">{{ item.name }}</span>
             </li>
         </ul>
-        <main grow h-full border-4 border-black overflow-auto>
-            <section v-for="(item, index) in dataSlice.list" :key="index" h-300 :id="item.key" border-2 b-black
+        <main grow h-full border-4 border-black overflow-auto ref="outBox">
+            <section v-for="(item, index) in dataSlice.list" :key="index" :id="item.id" h-200 border-2 b-black
                 ref="dataRef">
                 <div>{{ item.name }}</div>
                 <!-- component -->
@@ -25,9 +23,12 @@
     </div>
 </template>
 <script setup lang="ts">
+import { SateSet } from '@/components/common/miniExtension/utils/state'
 const monacoEditor = defineAsyncComponent(
     () => import('@/components/app/sys/blog/BlogEditor/monacoEditor.vue')
 )
+const outBox = ref<Element>()
+const sateSet = new SateSet()
 
 const dataRef = ref<Array<Element>>([])
 onMounted(() => {
@@ -40,67 +41,98 @@ onMounted(() => {
 // 所有列表
 const dataAll = [
     {
-        key: 'one1',
+        id: 'one1',
         name: 'one',
         component: monacoEditor
     },
     {
-        key: 'two2',
+        id: 'two2',
         name: 'two'
     },
     {
-        key: 'three3',
+        id: 'three3',
         name: 'three',
         component: monacoEditor
     },
     {
-        key: 'four4',
+        id: 'four4',
         name: 'four'
     },
     {
-        key: 'five5',
+        id: 'five5',
         name: 'five'
     }
 ]
 
 const dataFirst = dataAll[0]
 const dataSlice = ref({
-    active: dataFirst.key,
+    active: dataFirst.id,
     list: [dataFirst]
 })
 
-let inbdexStart = 0
-let indexInAll = 0
+let indexStart = 0
+let indexInDataAllOld = 0
 /**
  * 更新数据
  * @param data 对象
  * @param index 数组索引
  */
-const clickDataItem = (data: { key: any }, index: any) => {
-    indexInAll = index
-    const indexThis = indexInAll - 1
-    // 往前1个
-    inbdexStart = indexThis < 0 ? 0 : indexThis
-    dataRef.value.forEach((section) => {
+const clickDataItem = async (data: { id: any }, indexInDataAll: any) => {
+    console.log('click+i', data.id, indexInDataAll)
+    await renderAll(data.id, indexInDataAll)
+    // 滚动到指定位置  {behavior: "smooth"}顺滑
+    dataRef.value[indexInDataAllOld - indexStart].scrollIntoView({ behavior: "smooth" });
+}
+
+
+const renderAll = async (id: string, indexInDataAll: number) => {
+    // 左侧选中
+    dataSlice.value.active = id
+    // 更新div
+    renderDiv(indexInDataAll)
+    // 等待本次dom更新
+    await nextTick()
+    // 更新事件
+    gotoDiv()
+}
+
+/**
+ * 更新可视div
+ * @param index 当前索引
+ */
+const renderDiv = (indexInDataAll: number) => {
+    indexInDataAllOld = indexInDataAll
+    // 往前加载1个 后1个
+    const indexThis = indexInDataAllOld - 1
+    const indexEnd = indexInDataAllOld + 2
+    // 考虑首部0超限 实际位置
+    indexStart = indexThis < 0 ? 0 : indexThis
+
+    dataSlice.value.list = dataAll.slice(indexStart, indexEnd)
+}
+
+/**
+ * 更新事件
+ * 更新检测状态,滚动到指定位置
+ */
+const gotoDiv = async () => {
+    // 更新检测状态
+    const stateNew = sateSet.setNew(new Set(dataRef.value))
+    stateNew.delete.forEach((section) => {
         console.log('remove', section)
         observer.unobserve(section)
     })
-    dataSlice.value = {
-        active: data.key,
-        list: dataAll.slice(inbdexStart, indexInAll + 2)
-    }
-
-
-}
-
-onUpdated(() => {
-    console.log('onUpdate!!!!!!!!!!!!!!')
-    // 滚动到指定位置
-    dataRef.value[indexInAll - inbdexStart].scrollIntoView();
-    dataRef.value.forEach((section) => {
+    stateNew.add.forEach((section) => {
         console.log('add', section)
         observer.observe(section)
     })
+}
+
+
+onUpdated(() => {
+    console.log('onUpdate!!!!!!!!!!!!!!')
+
+
 })
 
 // 滚动监听
@@ -108,14 +140,30 @@ const options = {
     rootMargin: '-50% 0px',// y缩减一半
     threshold: 0
 }
-const observer = new IntersectionObserver((entries) => {
-    console.log('entries', entries)
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            console.log(entry.target.id)
-            dataSlice.value.active = entry.target.id
-        }
-    })
+/**
+ * 监听滚动
+ */
+const observer = new IntersectionObserver(async (entries) => {
+    // 遍历
+    for (let i = 0; i < entries.length; i++) {
+        const entry = entries[i]
+        if (!entry.isIntersecting) continue
+        const indexInDataAll = dataAll.findIndex((element) => element.id === entry.target.id);
+        // 跳转游标
+        const trans = indexInDataAll - indexInDataAllOld
+        if (trans == 0) break
+        let firstChangeDomHeight = null
+        // 下滚动 下滚动开始只有两个对象 下拉结尾未卸载第一个dom
+        if (trans == 1 && dataRef.value.length > 2) firstChangeDomHeight = dataRef.value[0]!.clientHeight
+        // 深拷贝
+        await renderAll(entry.target.id, indexInDataAll)  // 渲染后
+        // 上滚动
+        if (trans == -1) firstChangeDomHeight = dataRef.value[0]!.clientHeight
+        //dataRef.value.length>2 下拉结尾未添加尾部 上拉结尾未添加头部  不动
+        if (dataRef.value.length > 2 && firstChangeDomHeight)
+            outBox.value?.scrollTo({ top: outBox.value!.scrollTop - trans * firstChangeDomHeight })// 滚动
+        break
+    }
 }, options)
 </script>
 <style scoped></style>
