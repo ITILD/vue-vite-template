b37e5c71a361acf7490e90fe8510f5ca105b60f4
lpp <geolifestudy@gmail.com>
2024-08-16 16:23:20 +0800

    弹窗组件优化

--- web_vue/src/components/common/minUi/popup/MoveWindow.vue
+++ web_vue/src/components/common/minUi/popup/MoveWindow.vue
@@ -1,33 +1,53 @@
 <template>
   <div fixed ref="outDom">
-    <div class="con1_title" @mousedown="mouseDown($event)" @touchstart="touchDown($event)">
+    <div class="con1_title" @mousedown="mouseOrTouchDown($event)" @touchstart="mouseOrTouchDown($event)">
       <!-- <div>标题111，拖拽它即可使整个div移动</div> -->
-       <slot></slot>
+      <slot></slot>
     </div>
-    <!-- <div>这是内容</div> -->
+    <!-- 内容插槽 -->
+    <slot name="content"></slot>
   </div>
 </template>
 <script setup lang="ts">
+/**
+ * 鼠标按下,不经过移动 moveEl.dragFlag = false;
+ * 用法
+ * 
+    <MoveWindow ref="dragRef" right-8 bottom-30>
+        <div w-30 h-30 rounded-3xl flex-center>
+            <div w-full h-full @click="addPage">新建</div>
+        </div>
+        <template v-slot:content>
+          <div w-30 h-30 rounded-3xl flex-center >内容</div>
+        </template>
+    </MoveWindow>
+    const dragRef = ref();
+    const addPage = () => {
+    if(dragRef.value.$el.dragFlag)return
+    console.log('addPage')
+    }
+ */
 const outDom = ref()
 
-const mouseDown = (e: MouseEvent) => {
+const mouseOrTouchDown = (e: MouseEvent | TouchEvent) => {
   let moveEl: any = outDom.value
-  // 鼠标的位置 e.clientX e.clientY
-  // 元素的位置 moveEl.offsetLeft moveEl.offsetTop
+  //拖拽标记还原 阻止外部点击
+  moveEl.dragFlag = false;
   // 元素的大小
   let domW = moveEl.offsetWidth
   let domH = moveEl.offsetHeight
   // 窗口大小
   let width = window.innerWidth
   let height = window.innerHeight
-
   // 鼠标到元素左边距离
-  let moveX = e.clientX - moveEl.offsetLeft
-  let moveY = e.clientY - moveEl.offsetTop
-  document.onmousemove = (e) => {
-    let dropX = width - (e.clientX - moveX) - domW
-    let dropY = height - (e.clientY - moveY) - domH
-
+  let moveX = getMouseOrTouchXY(e).x - moveEl.offsetLeft
+  let moveY = getMouseOrTouchXY(e).y - moveEl.offsetTop
+  const onMove = (e: MouseEvent | TouchEvent) => {
+    console.log('l2', e.type)
+    //设置拖拽标记   
+    moveEl.dragFlag = true;
+    let dropX = width - (getMouseOrTouchXY(e).x - moveX) - domW
+    let dropY = height - (getMouseOrTouchXY(e).y - moveY) - domH
     if (dropX > width - domW) {
       dropX = width - domW
     } else if (dropX < 0) {
@@ -41,56 +61,47 @@ const mouseDown = (e: MouseEvent) => {
     moveEl.style.right = dropX + 'px'
     moveEl.style.bottom = dropY + 'px'
   }
-  document.onmouseup = () => (document.onmousemove = document.onmouseup = null)
+  // 判断触摸还是鼠标
+  if (e instanceof MouseEvent) {
+    document.onmousemove = onMove
+    document.onmouseup = () => {
+      console.log('MouseEvent抬起')
+      document.onmousemove = document.onmouseup = null
+    }
+    // 鼠标
+  } else {
+    console.log('l1', e.type)
+    // 触摸 移动端失效
+    // document.ontouchmove = onMove
+    // document.ontouchend = () => (document.ontouchmove = document.ontouchend = null)
+    const touchend = () => {
+      // console.log('touchDown抬起')
+      document.removeEventListener('touchmove', onMove)
+      document.removeEventListener('touchend', touchend)
+      moveEl.dragFlag = false;
+    }
+    document.addEventListener('touchmove', onMove)
+    document.addEventListener('touchend', touchend)
+  }
   // 防止鼠标松开元素仍然可以拖动;
   document.ondragstart = document.ondragend = (ev) => ev.preventDefault()
 }
-
-const touchDown = (e: TouchEvent) => {
-  let moveEl: any = outDom.value
-  // 鼠标的位置 e.clientX e.clientY
-  // 元素的位置 moveEl.offsetLeft moveEl.offsetTop
-  // 元素的大小
-  let domW = moveEl.offsetWidth
-  let domH = moveEl.offsetHeight
-  // 窗口大小
-  let width = window.innerWidth
-  let height = window.innerHeight
-  console.log('按下touchDown')
-  // 鼠标到元素左边距离
-  let moveX = e.targetTouches[0].pageX - moveEl.offsetLeft
-  let moveY = e.targetTouches[0].pageY  - moveEl.offsetTop
-
-  const touchmove = (e: TouchEvent) => {
-    console.log('touchDown移动')
-    let dropX = width - (e.targetTouches[0].pageX  - moveX) - domW
-    let dropY = height - (e.targetTouches[0].pageY  - moveY) - domH
-
-    if (dropX > width - domW) {
-      dropX = width - domW
-    } else if (dropX < 0) {
-      dropX = 0
-    }
-    if (dropY > height - domH) {
-      dropY = height - domH
-    } else if (dropY < 0) {
-      dropY = 0
-    }
-    moveEl.style.right = dropX + 'px'
-    moveEl.style.bottom = dropY + 'px'
+/**
+ * 获取鼠标或触摸坐标
+ * @param e 
+ */
+const getMouseOrTouchXY = (e: MouseEvent | TouchEvent) => {
+  let x = null, y = null
+  // 判断触摸还是鼠标
+  if (e instanceof MouseEvent) {
+    x = e.clientX
+    y = e.clientY
+  } else {
+    x = e.targetTouches[0].pageX
+    y = e.targetTouches[0].pageY
   }
-  const touchend = () => {
-    console.log('touchDown抬起')
-    document.removeEventListener('touchmove', touchmove)
-    document.removeEventListener('touchend', touchend)
-  }
-  document.addEventListener('touchmove', touchmove)
-  document.addEventListener('touchend', touchend)
-
-  // 防止鼠标松开元素仍然可以拖动;
-  document.ondragstart = document.ondragend = (ev) => ev.preventDefault()
+  return { x, y }
 }
-
 
 
 </script>
--- web_vue/src/views/home_page.vue
+++ web_vue/src/views/home_page.vue
@@ -1,27 +1,27 @@
 <template>
   <div grid-head-center-foot>
-    <SysHeader h-14 bg-deep-1 z-999  shadow shadow-gray/>
+    <SysHeader h-14 bg-deep-1 z-999 shadow shadow-gray />
     <!-- :style="'clip-path: polygon(0 0, 100% 0, 100% 100%, 0 '+clipPath[0]+'%)'"  -->
-    <main bg-deep-3 px-1 py-2 >
-      <div bg-deep-2 h-full rounded-md p-2 >
+    <main bg-deep-3 px-1 py-2>
+      <div bg-deep-2 h-full rounded-md p-2>
         <!-- 类似知乎抖音刷 -->
-        <div >
-          图数据库选择 下拉 大类   跳转后子类
+        <div>
+          图数据库选择 下拉 大类 跳转后子类
 
 
           考虑桌面风格文件和列表
         </div>
-        
-        <div >
+
+        <div>
           过滤
           list
         </div>
-        
-        <div >页码  </div>
-        <div> 地球 飞船 黑洞     编辑 新建 删除</div>
+
+        <div>页码 </div>
+        <div> 地球 飞船 黑洞 编辑 新建 删除</div>
 
         <div>语义化Semanticization</div>
-        <div>设计代码器 2  editblog后面</div>
+        <div>设计代码器 2 editblog后面</div>
 
 
         <!-- https://github.com/google/diff-match-patch   diff比较  前后端 -->
@@ -31,16 +31,17 @@
 
       </div>
 
-      <MoveWindow  right-8 bottom-30 >
-        <div w-30 h-30 rounded-3xl flex-center btn-deep-6 shadow-md shadow-gray > 
+      <MoveWindow ref="dragRef" right-8 bottom-30>
+        <div w-30 h-30 rounded-3xl flex-center btn-deep-6 shadow-md shadow-gray>
 
-          <!-- <div  @click="addPage">新建</div> -->
+          <div w-full h-full @click="addPage">新建</div>
           <!-- /blog/blog-new 路由 -->
-           <RouterLink font-600 text-4xl  font-serif  to="/blog/blog-new" >新建</RouterLink>
+          <!-- <RouterLink font-600 text-4xl  font-serif  to="/blog/blog-new" >新建</RouterLink> -->
+
         </div>
-       </MoveWindow>
+      </MoveWindow>
     </main>
-    <SysFooter  mx-auto w-full p-2 lg:py-2 bg-deep-0  />
+    <SysFooter mx-auto w-full p-2 lg:py-2 bg-deep-0 />
   </div>
 </template>
 
@@ -48,13 +49,19 @@
 import { defineComponent, ref, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import { SysSettingStore } from '@/stores/sys'
+import router from '@/router';
 const { sysStyle } = storeToRefs(SysSettingStore())
 sysStyle.value.headShow = true
 console.log('跳转')
-const clipPath =ref( [100])
+const clipPath = ref([100])
 // offset-path是用来实现路径动画  可以考虑流星浪花藤曼
+
+const dragRef = ref();
 const addPage = () => {
-  console.log('addPage')
+  // 拖拽中，不执行后续操作
+  if (dragRef.value.$el.dragFlag) return
+  // 导航到 /blog/blog-new
+  router.push('/blog/blog-new')
 }
 </script>
 <style scoped></style>
