bdce742b4e4e37ac4756246f31152923bf3bbad9
lpp <geolifestudy@gmail.com>
2024-09-05 17:34:11 +0800

    db修改

--- server_py/src/config/db.py
+++ server_py/src/config/db.py
@@ -1,4 +1,5 @@
 # self
+
 from config.log import console
 
 # lib
@@ -12,55 +13,75 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.pool import QueuePool
 from sqlalchemy.orm import sessionmaker
 
+# baselib
+from abc import ABC, abstractmethod
 
-# 枚举类
-class DataLoaderType:
-    sqlite = "sqlite+aiosqlite"
-    postgresql = "postgresql+asyncpg"
-    # mysql = "mysql+aiomysql://"
+# # 枚举类
+# class DataBaseType:
+#     sqlite = "sqlite+aiosqlite"
+#     postgresql = "postgresql+asyncpg"
+#     # mysql = "mysql+aiomysql://"
 
 
-class DataLoaderInterface:
-
+# 数据接口类
+class DataBaseInterface(ABC):
+    @abstractmethod
     def __init__(self):
         raise NotImplementedError
 
     def connect(self):
         raise NotImplementedError
 
-class SqlConnect:
+    def disconnect(self):
+        raise NotImplementedError
+
+
+class DataBaseSqlite(DataBaseInterface):
+    url: str = None
+    engine: create_async_engine = None
+    sessionLocal: sessionmaker = None
+    
+    def __init__(self, path: str):
+        # 打包跟随程序
+        type = "sqlite+aiosqlite"
+        self.url = f"{type}:///{path}"
+
+    def connect(self):
+        # 控制台打印SQL
+        engine = engine = create_async_engine( self.url, echo=True )
+        self.sessionLocal =  sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+        
+    def disconnect(self):
+        print("sqlite disconnect")
+
+
+class DataBasePostgre(DataBaseInterface):
     url: str = None
     pwd: str = None
     host: str = None
     port: int = None
     engine: create_async_engine = None
-    def __init__(
-        self, type: DataLoaderType, path: str, user: str, pwd: str, host: str, port: int
-    ):
-        # 打包跟随程序
-        if type == DataLoaderType.sqlite:
-            self.url = f"{type}:///{path}"
-        # 远程
-        elif type == DataLoaderType.postgresql:
-            # postgresql+asyncpg://hero:heroPass123@0.0.0.0:5432/heroes_db
-            self.url = f"{type}://{user}:{pwd}@{host}:{port}/{path}"
+    sessionLocal: sessionmaker = None
+    
+    def __init__(self, path: str, user: str, pwd: str, host: str, port: int):
+        type = "postgresql+asyncpg"
+        # postgresql+asyncpg://hero:heroPass123@0.0.0.0:5432/heroes_db
+        self.url = f"{type}://{user}:{pwd}@{host}:{port}/{path}"
         # todo: mysql
         #  "mysql+pymysql://user:password@host:port/database"
-    def connect(self, url: str):
-        self.engine = url
-
+    def connect(self):
+        # 控制台打印SQL
+        engine = engine = create_async_engine( self.url, echo=True )
+        self.sessionLocal =  sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+    def disconnect(self):
+        print("postgresql disconnect")
 
 SQLALCHEMY_DATABASE_URL = "source/db/base.db"
-SQL_TYPE_SQLITE = "sqlite+aiosqlite:///"
 
+dataBaseSqliteDeafault = DataBaseSqlite(SQLALCHEMY_DATABASE_URL)
+dataBaseSqliteDeafault.connect()
 
-
-engine = create_async_engine(
-    SQL_TYPE_SQLITE + SQLALCHEMY_DATABASE_URL,
-    echo=True,  # 控制台打印SQL
-)
-
-sessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+sessionLocalUse = dataBaseSqliteDeafault.sessionLocal
 
 
 def Data(f):
@@ -70,7 +91,7 @@ def Data(f):
     async def wrapper(*args, **kwargs):
         try:
             # 创建一个配置过的Session类
-            async with sessionLocal() as session:  # 确保 session 总是被关闭
+            async with sessionLocalUse() as session:  # 确保 session 总是被关闭
                 # 设置session类型
                 kwargs["session"] = session  # 将 session 作为关键字参数传递给 f
                 result = await f(*args, **kwargs)
@@ -91,7 +112,7 @@ def DataNoCommit(f):
     async def wrapper(*args, **kwargs):
         try:
             # 创建一个配置过的Session类
-            async with sessionLocal() as session:  # 确保 session 总是被关闭
+            async with sessionLocalUse() as session:  # 确保 session 总是被关闭
                 # 设置session类型
                 kwargs["session"] = session  # 将 session 作为关键字参数传递给 f
                 result = await f(*args, **kwargs)
@@ -103,5 +124,7 @@ def DataNoCommit(f):
 
     return wrapper
 
+
+# 管理器
 
 console.log("...数据库配置完成")
