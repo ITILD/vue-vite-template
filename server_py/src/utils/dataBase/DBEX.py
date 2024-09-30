8b3e948f3258d4a065ba4d8302276b3ced3a527c
lpp <geolifestudy@gmail.com>
2024-09-30 17:31:12 +0800

    自动sql

--- server_py/src/utils/dataBase/DBEX.py
+++ server_py/src/utils/dataBase/DBEX.py
@@ -6,20 +6,71 @@ from config.db import Data, DataNoCommit
 class DBEX:
     def controller_init(routerThis, description, DoService, Do, DoCreate) -> str:
         @routerThis.post(
-            "/default_add",
+            "/default",
             status_code=status.HTTP_201_CREATED,
             summary=f"添加{description}返回id",
         )
         async def add(do: DoCreate) -> str:
             return await DoService.add(do)
 
+        @routerThis.delete(
+            "/default",
+            summary=f"删除{description}",
+        )
+        async def delete(id: str):
+            return await DoService.delete(id)
+
+        @routerThis.put(
+            "/default",
+            summary=f"更新{description}",
+        )
+        async def update(do: Do):
+            await DoService.update(do)
+
+        @routerThis.get(
+            "/default",
+            status_code=status.HTTP_201_CREATED,
+            summary=f"获取单个{description}",
+        )
+        async def select(id: str) -> Do | None:
+            return await DoService.select(id)
+
+        @routerThis.get(
+            "/default/list",
+            status_code=status.HTTP_201_CREATED,
+            summary=f"获取批量{description}",
+        )
+        async def list():
+            return await DoService.list()
+
     # service初始化
     def service_init(DoService, DoDao, Do, DoCreate):
-        # @classmethod
         @staticmethod
         async def add(do: DoCreate) -> str:
             return await DoDao.add(do)
         DoService.add = add
+        
+        
+        @staticmethod
+        async def delete(id: str):
+            await DoDao.delete(id)
+        DoService.delete = delete
+
+        @staticmethod
+        async def update(do: Do):
+            await DoDao.update(do)
+        DoService.update = update
+            
+        @staticmethod
+        async def select(id: str) -> Do | None:
+            return await DoDao.select(id)
+        DoService.select = select
+        
+        @staticmethod
+        async def list():
+            return await DoDao.list()
+        DoService.list = list
+
 
     def dao_init(DoDao, Do, DoCreate):
 
@@ -30,4 +81,5 @@ class DBEX:
             session.add(db_do)
             await session.commit()
             return db_do.id
+
         DoDao.add = add
