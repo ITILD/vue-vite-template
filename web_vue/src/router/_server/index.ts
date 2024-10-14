25ea63bb504b333684a12cf8ac80deadf353722b
lpp <geolifestudy@gmail.com>
2024-10-14 17:28:25 +0800

    服务管理

--- web_vue/src/router/_server/index.ts
+++ web_vue/src/router/_server/index.ts
@@ -1,5 +1,12 @@
 const routerServer = [
   // 已添加
-  { path: '/_server', component: () => import('@/views/_server/index-page.vue') },
+  {
+    path: '/_server', component: () => import('@/views/_server/index-page.vue'),
+
+    children: [
+      { path: 'dev_state', component: () => import('@/views/_server/index-page/dev_state.vue') },
+      { path: 'server_state', component: () => import('@/views/_server/index-page/server_state.vue') },
+    ]
+  },
 ]
 export { routerServer }
--- web_vue/src/views/_server/index-page.vue
+++ web_vue/src/views/_server/index-page.vue
@@ -1,9 +1,11 @@
 <template>
-    <div>
+    <!-- <div absolute w-full h-full flex bg-deep-1> -->
         <!-- 后台服务 -->
         <!-- 概览(详情内部分组件  数据库表和数目统计)-->
         <!-- 详情跳转 -->
-    </div>
+         <!-- todo 传入 -->
+        <MenuTabs absolute w-full h-full />
+    <!-- </div> -->
 </template>
 
 <script setup lang="ts">
