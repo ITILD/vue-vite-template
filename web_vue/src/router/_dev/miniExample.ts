231c6da5618a2ec89129ab2e6880559aee068954
lpp <geolifestudy@gmail.com>
2024-10-18 16:07:59 +0800

    mediapipe

--- web_vue/src/router/_dev/miniExample.ts
+++
@@ -1,5 +0,0 @@
-const routerMiniExample = [
-  // 路由首页
-  { path: '/_dev', component: () => import('@/views/_dev/index-page.vue') }
-]
-export { routerMiniExample }
--- web_vue/src/views/blog/blog-new-page.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>template  todo 简易正则 和公式解析</div>
-  <BlogEditor w-full h-300 />
-  <BlogShow />
-</template>
-<script setup lang="ts">
-</script>
-<style scoped></style>
